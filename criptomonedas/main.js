bf0013c6c5c1b9967a2276438724472954f091c2
Daniel Campoverde <alx@sillybytes.net>
2021-12-27 09:31:34 -0500

    Deploy

--- criptomonedas/main.js
+++ criptomonedas/main.js
@@ -4,230 +4,234 @@ console.log("\t https://www.kaggle.com/sudalairajkumar/cryptocurrencypricehistor
 console.log("\t https://www.kaggle.com/jessevent/all-crypto-currencies");
 
 
-
-const transparency    = 'AA';
-const scaling_logbase = 2;
-const scaling_linear  = 8;
-const min_r           = 5;
-
-const color = {
+new fullpage('#fullpage', {
+    autoScrolling: true,
+});
 
 
-    Aave:          '#B6509E',
-    BinanceCoin:   '#F3BB2B',
-    Bitcoin:       '#F79413',
-    Cardano:       '#0030AF',
-    Chainlink:     '#345BD3',
-    Cosmos:        '#2E3148',
-    CryptocomCoin: '#02154B',
-    Dogecoin:      '#BBA030',
-    EOS:           '#000000',
-    Ethereum:      '#626890',
-    Generic:       '#2855AA',
-    IOTA:          '#242424',
-    Litecoin:      '#315D9E',
-    Monero:        '#FF6600',
-    NEM:           '#4FBAAD',
-    Polkadot:      '#E30475',
-    Solana:        '#28ACAA',
-    Stellar:       '#000000',
-    TRON:          '#FF0013',
-    Tether:        '#26A17B',
-    USDCoin:       '#3ACC8A',
-    Uniswap:       '#EC4297',
-    XRP:           '#222222',
-};
 
-Chart.register(ChartDataLabels);
 
-const config = {
-    type: 'bubble',
+// ===============================
+// Coins by year
+// ===============================
+
+const config_coins_by_year = {
+    type: 'bar',
     data: {
-        labels: [1, 2],
-        datasets: [],
+        labels: ['2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', 'Enero 2021', 'Noviembre 2021'],
+        datasets: [{
+            label: "Número de Criptomonedas",
+            data: [7, 67, 501, 572, 636, 1359, 2086, 2403, 4154, 6632],
+            backgroundColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.3)',
+                'rgba(54, 162, 235, 0.4)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.9)',
+                'rgba(54, 162, 235, 1)',
+            ],
+        }]
     },
     options: {
         responsive: true,
-        layout: {
-            padding: 20,
-        },
         scales: {
-            y: { title: "test", display: true, beginAtZero: true, type: 'logarithmic', min: 0, max: 1000000},
-            x: { title: "test", display: true, beginAtZero: true, type: 'logarithmic', min: 0, max: 100000},
-        },
-        plugins: {
-            title: {
-                display: true,
-                text: "Criptomonedas",
-                font: {
-                    size: 40
-                }
-            },
-
-            datalabels: {
-                anchor: function(context) {
-                    var value = context.dataset.data[context.dataIndex];
-                    return value.r < 15 ? 'end' : 'center';
-                },
-                align: function(context) {
-                    var value = context.dataset.data[context.dataIndex];
-                    return value.r < 15 ? 'end' : 'center';
-                },
-                color: function(context) {
-                    var value = context.dataset.data[context.dataIndex];
-                    return value.r < 15 ? context.dataset.backgroundColor : 'white';
-                },
-                font: {
-                    weight: 'bold'
-                },
-                formatter: function(value) {
-                    if (value.r > 0 ) {
-                        let usd = Math.round(scaling_logbase ** ((value.r - min_r) / scaling_linear));
-                        if (usd > 1) {
-                            return "$ " + usd;
-                        } else {
-                            return "< $ 1";
-                        }
-                    } else {
-                        return "0";
-                    }
-                },
-                offset: 2,
-                padding: 0
+            y: {
+                beginAtZero: true
             }
         }
     }
 };
 
-const chart = new Chart(document.getElementById('chart'), config);
+const chart_coins_by_year = new Chart(document.getElementById('chart-coins-by-year'), config_coins_by_year);
 
 
-function clearChart(chart) {
-    chart.data.labels = [];
-    chart.data.datasets = [];
-}
-
-function insert_coin_dataset(chart, coinName, value, volume, marketcap) {
-    let coinDataset = {
-        label: coinName,
-        data: [{r: value, x: volume, y: marketcap}],
-        backgroundColor: color[coinName] + transparency,
-        borderWidth: 1,
-        clip: {left: false, top: false, right: false, bottom: false},
-    };
-
-    chart.data.datasets.push(coinDataset);
-}
 
 
-function insert_or_update_coin_dataset(chart, coinName, value, volume, marketcap) {
-    let datasets = chart.data.datasets;
-    for (var i in datasets) {
-        if (datasets[i].label == coinName) {
-            // Update dataset
-            console.log("Updating " + coinName);
-            datasets[i].data = [{r: value, x: volume, y: marketcap}];
-            return;
+
+// ===============================
+// Users by continent
+// ===============================
+
+const config_users_by_continent = {
+    type: 'bar',
+    data: {
+        labels: ['Oceania', 'Sudamérica', 'Norteamérica', 'África', 'Europa', 'Asia'],
+        datasets: [{
+            label: "Número de Usuarios (millones)",
+            data: [1, 24, 28, 32, 38, 160],
+            backgroundColor: [
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(75, 192, 192, 0.4)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(75, 192, 192, 0.9)',
+                'rgba(75, 192, 192, 1)',
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        indexAxis: 'y',
+        scales: {
+            y: {
+                beginAtZero: true
+            }
         }
     }
+};
 
-    console.log("Inserting " + coinName);
-    insert_coin_dataset(chart, coinName, value, volume, marketcap);
-}
+const chart_users_by_continent = new Chart(document.getElementById('chart_users_by_continent'), config_users_by_continent);
 
-function to_human_month(nString) {
-    let n = parseInt(nString);
 
-    switch (n) {
-        case 1:  return "Enero";
-        case 2:  return "Febrero";
-        case 3:  return "Marzo";
-        case 4:  return "Abril";
-        case 5:  return "Mayo";
-        case 6:  return "Junio";
-        case 7:  return "Julio";
-        case 8:  return "Agosto";
-        case 9:  return "Septiembre";
-        case 10: return "Octubre";
-        case 11: return "Noviembre";
-        case 12: return "Diciembre";
+
+
+
+// ===============================
+// Algoritmos
+// ===============================
+
+const config_algoritmos = {
+    type: 'polarArea',
+    data: {
+        labels: ['Scrypt', 'SHA256', 'Ethash', 'CyptoNight', 'Otros'],
+        datasets: [{
+            label: "Algoritmos de hashing populares",
+            data: [84, 49, 34, 32, 44],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 159, 64, 0.7)',
+                'rgba(255, 205, 86, 0.7)',
+                'rgba(75, 192, 192, 0.7)',
+                'rgba(201, 203, 207, 0.7)',
+            ],
+        }]
+    },
+};
+
+const chart_algoritmos = new Chart(document.getElementById('chart_algoritmos'), config_algoritmos);
+
+
+
+
+
+// ===============================
+// Energy
+// ===============================
+
+const config_energy = {
+    type: 'bar',
+    data: {
+        labels: ['Prueba de cobertura (WiFi)', 'Prueba de espacio (HDDs, SSDs)', 'Prueba de Participación (CPU)',
+            'Prueba de Trabajo (CPU)', 'Prueba de Trabajo (GPU)', 'Prueba de Trabajo (ASIC)'
+        ],
+        datasets: [{
+            label: "Energía promedio por nodo de minado",
+            data: [10, 14, 55, 230, 290, 850],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(255, 205, 86, 0.8)',
+                'rgba(255, 159, 64, 0.8)',
+                'rgba(255, 99, 132, 0.8)',
+            ],
+        }]
+    },
+    options: {
+        layout: {
+            padding: 20,
+        },
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            deferred: {
+                xOffset: 150,   // defer until 150px of the canvas width are inside the viewport
+                yOffset: '50%', // defer until 50% of the canvas height are inside the viewport
+                delay: 500      // delay of 500 ms after the canvas is considered inside the viewport
+            }
+        },
     }
-}
+};
 
-function logBase(x, base) {
-    return Math.log(x) / Math.log(base);
-}
-
-function scaleRadious(x) {
-    if (x > 1) {
-        return (min_r + ((logBase(x, scaling_logbase) * scaling_linear)));
-    } else {
-        return min_r;
-    }
-}
-
-// Date: year.month
-//       2013.04
-function apply_data_for_date(chart, date) {
-    newData = monedas[date];
-    console.log(newData);
-
-    for (var i in newData) {
-        let datum = newData[i];
-        insert_or_update_coin_dataset(chart,
-            datum['coin'],
-            scaleRadious(datum['value']),
-            datum['volume'] / 1000000,
-            Math.round(datum['marketcap']) / 1000000
-        );
-    }
-
-    chart.update();
-
-    let split = date.split('.');
-    let year  = split[0];
-    let month = split[1];
-    document.getElementById('year').textContent = year;
-    document.getElementById('month').textContent = to_human_month(month);
-}
+const chart_energy = new Chart(document.getElementById('chart_energy'), config_energy);
 
 
-function increment_date(date) {
-    let maxYear = 2021;
-    let maxMonth = 7;
 
-    let split = date.split('.');
-    let year  = parseInt(split[0]);
-    let month = parseInt(split[1]);
 
-    var newYear = year;
-    var newMonth = month;
 
-    // if (year < maxYear) {
-    //     newYear += 1;
-    // }
+// ===============================
+// Comparacion
+// ===============================
 
-    if (month < 12) {
-        newMonth += 1;
-    } else {
-        newMonth = 1;
-        if (year < maxYear) {
-            newYear += 1;
+const config_comparacion = {
+    type: 'radar',
+    data: {
+        labels: ['Valor relativo(log)', 'Energia requerida por nodo', 'Indice de privacidad relativa',
+            'Indice de popularidad', 'Costo de hardware de minado por nodo (log)'
+        ],
+        datasets: [
+            {
+                label: "Bitcoin",
+                data: [100, 100, 20, 90, 100],
+                fill: true,
+                backgroundColor:           '#F7941322',
+                borderColor:               '#F79413',
+                pointHoverBorderColor:     '#F79413',
+                pointBackgroundColor:      '#F79413',
+                pointBorderColor:          '#FFF',
+                pointHoverBackgroundColor: '#FFF',
+            },
+
+            {
+                label: "Monero",
+                data: [15, 40, 100, 20, 20],
+                fill: true,
+                backgroundColor:           '#315D9E22',
+                borderColor:               '#315D9E',
+                pointHoverBorderColor:     '#315D9E',
+                pointBackgroundColor:      '#315D9E',
+                pointBorderColor:          '#FFF',
+                pointHoverBackgroundColor: '#FFF',
+            },
+
+            {
+                label: "Chia",
+                data: [10, 10, 20, 10, 10],
+                fill: true,
+                backgroundColor:           '#6ec0a722',
+                borderColor:               '#6ec0a7',
+                pointHoverBorderColor:     '#6ec0a7',
+                pointBackgroundColor:      '#6ec0a7',
+                pointBorderColor:          '#FFF',
+                pointHoverBackgroundColor: '#FFF',
+            },
+
+        ]
+    },
+    options: {
+        scales: {
+            r: {
+                angleLines: {
+                    display: false
+                },
+                suggestedMin: 1,
+                suggestedMax: 100
+            }
+        },
+        elements: {
+            line: {
+                borderWidth: 3
+            }
         }
     }
+};
 
-    if (newMonth < 10) {
-        return newYear + '.0' + newMonth;
-    } else {
-        return newYear + '.' + newMonth;
-    }
-}
-
-var currentDate = "2013.04";
-apply_data_for_date(chart, currentDate);
-
-function next() {
-    currentDate = increment_date(currentDate);
-    console.log(currentDate);
-    apply_data_for_date(chart, currentDate);
-}
+const chart_comparacion = new Chart(document.getElementById('chart_comparacion'), config_comparacion);
